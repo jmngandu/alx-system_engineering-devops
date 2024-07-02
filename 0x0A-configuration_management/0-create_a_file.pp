c26330fdaf5386c594e48921cb481819d75eb339
jmngandu <jamesmngandu@gmail.com>
2024-07-02 19:37:11 +0300

    configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates a file in the tmp diirectory of 
+# the node 
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
