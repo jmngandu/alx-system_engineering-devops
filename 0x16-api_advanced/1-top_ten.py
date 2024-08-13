543da4751054cd40fcf9c07e020a5d3452cf3de0
jmngandu <jamesmngandu@gmail.com>
2024-08-13 12:40:53 +0300

    API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
+
 """
-queries the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit.
+query the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
-import json
-import requests
+
+from requests import get
+from sys import argv
 
 
-def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+def top_ten(subreddit: str) -> None:
+    """
+    function that does the heavy lifting for us
+    Args:
+        subreddit (str) -> The subreddit to query
+    Returns: The top 10 hots for that subreddit
+    """
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Marvel's Agents of Shield/21",
+        "X-Forwared-For": "Phil J. Coulson"
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+
+    request_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    try:
+        response = get(request_url, headers=headers,
+                       allow_redirects=False).json()
+        data = response['data']['children']
+        [print(post['data']['title']) for post in data[:10]]
+    except Exception:
         print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+
+if __name__ == "__main__":
+    (top_ten(argv[1]))
