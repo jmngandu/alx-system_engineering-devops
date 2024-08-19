f8b61dd8d86131e940e16dd5d5650f73a570f9b8
jmngandu <jamesmngandu@gmail.com>
2024-08-19 22:31:24 +0300

    web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env puppet
+
+# patch up our nginx webserver to handle
+# a large number of requests
+exec { 'fix nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# restart Nginx service (nginx.service)
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
