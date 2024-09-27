85a4f314190fd422ac4b0d43a33dc843bab82665
jmngandu <jamesmngandu@gmail.com>
2024-09-27 03:22:24 -0700

    how many subs task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
+"""This is a fuinction that queries the Reddit API and returns the
+number of subscribers for a given subreddit"""
 
-"""
-Query a subreddit and return the number of
-total subscribers in that subredit
-"""
-
-from requests import get
-from sys import argv
+import requests
 
 
-headers = {
-    "User-Agent": "Of course I had to use a custom User-Agent",
-    "X-Forwared-For": "iamthecavalry"
-}
-
-
-def number_of_subscribers(subreddit: str) -> int:
-    """
-    Query the subreddit and return the number of
-    Active subs. If its an invalid subredit, return 0
-    """
-    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                   headers=headers)
-    data = response.json()
-    try:
-        if 'error' in data.keys():
-            return 0
-        else:
-            return data['data']['subscribers']
-    except Exception as e:
-        return 0
-
-
-if __name__ == "__main__":
-    print(number_of_subscribers(argv[1]))
+def number_of_subscribers(subreddit):
+	""" queries the Reddit API and returns the number of
+	subscribers for a given subreddit"""
+    
+	url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+	headers = {'User-Agent': 'My User Agent 1.0'}
+	response = requests.get(url, headers=headers)
+	if response.status_code == 200:
+		data = response.json()
+		return data.get('data').get('subscribers')
+	else:
+		return 0
