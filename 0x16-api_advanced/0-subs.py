6c61f33f8783047221a1694a547665a36bd0e633
jmngandu <jamesmngandu@gmail.com>
2024-09-27 09:50:09 -0700

    Revert "tasks"
    This reverts commit 8ceff39d9cb437414ec7187d2173f76a5a84830b.
    
    revert

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API to get the no of subscribers for given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: no of subscribers or 0 if the subreddit is invalid.
-    """
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "jthree3_globally:v1.0.1 (by /u/jmngandu)"
+        "User-Agent": "jthree_globally:v1.0.0 (by /u/jmngandu)"
     }
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 404:
         return 0
-
-    try:
-        data = response.json()
-        return data["data"]["subscribers"]
-    except (KeyError, json.JSONDecodeError):
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,36 @@
-import requests
+#!/usr/bin/python3
 
-def number_of_subscribers(subreddit):
+"""
+query the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
+from requests import get
+from sys import argv
+
+
+def top_ten(subreddit: str) -> None:
     """
-    Queries the Reddit API to get the number of subscribers 
-
+    function that does the heavy lifting for us
     Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers for the subreddit,.
+        subreddit (str) -> The subreddit to query
+    Returns: The top 10 hots for that subreddit
     """
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "jthree3_globally:v1.0.1 (by /u/jmngandu)"
+        "User-Agent": "Marvel's Agents of Shield/21",
+        "X-Forwared-For": "Phil J. Coulson"
     }
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 404:
-        return 0
+    request_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
     try:
-        data = response.json()
-        return data["data"]["subscribers"]
-    except (KeyError, json.JSONDecodeError):
-        return 0
+        response = get(request_url, headers=headers,
+                       allow_redirects=False).json()
+        data = response['data']['children']
+        [print(post['data']['title']) for post in data[:10]]
+    except Exception:
+        print("None")
+
+
+if __name__ == "__main__":
+    (top_ten(argv[1]))
