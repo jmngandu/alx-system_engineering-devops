4336df758627e6195b06348e87595835651e3ec7
jmngandu <jamesmngandu@gmail.com>
2024-09-27 09:16:59 -0700

    task 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "jmngandu:jthree_globally-project3"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
